5afd2ea3195fd1bd66d246b04418289eab4e3f6a
TheBlackParrot <theblackparrot0@gmail.com>
2025-06-29 18:53:05 -0500

    clean up some stuff

--- GameCoverColors/Extensions/ColorExtensions.cs
+++ GameCoverColors/Extensions/ColorExtensions.cs
@@ -4,44 +4,51 @@ namespace GameCoverColors.Extensions;
 
 public static class Color
 {
-    private static float MinColorComponent(this UnityEngine.Color color) => Mathf.Max(0.001f, Mathf.Min(Mathf.Min(color.r, color.g), color.b));
     public static float GetYiq(this UnityEngine.Color color) => (color.r * 299) + (color.g * 587) + (color.b * 114);
     
     public static float GetHue(this UnityEngine.Color color)
     {
-        float max = Mathf.Max(Mathf.Max(color.r, color.g), color.b);
-        float min = Mathf.Min(Mathf.Min(color.r, color.g), color.b);
-        float chroma = max - min;
-        float h1;
+        float min = Mathf.Min(color.r, color.g, color.b);
+        float max = Mathf.Max(color.r, color.g, color.b);
+        float delta = max - min;
         
-        if (chroma == 0)
-            h1 = 0;
+        float hue;
+        if (delta == 0)
+            hue = 0;
         else if (Mathf.Approximately(max, color.r))
-            h1 = (color.g - color.b) / chroma % 6;
+            hue = (color.g - color.b) / delta % 6;
         else if (Mathf.Approximately(max, color.g))
-            h1 = 2 + (color.b - color.r) / chroma;
+            hue = 2 + (color.b - color.r) / delta;
         else
-            h1 = 4 + (color.r - color.g) / chroma;
+            hue = 4 + (color.r - color.g) / delta;
 
-        return 60 * h1;
+        return 60 * hue;
     }
 
     public static float GetSaturation(this UnityEngine.Color color)
     {
-        float max = Mathf.Max(Mathf.Max(color.r, color.g), color.b);
-        float min = Mathf.Min(Mathf.Min(color.r, color.g), color.b);
-        float chroma = max - min;
+        float min = Mathf.Min(color.r, color.g, color.b);
+        float max = Mathf.Max(color.r, color.g, color.b);
+        float saturation = (min + max) / 2;
+
+        if(Mathf.Approximately(min, max)) {
+            saturation = 0;
+        }
+        else
+        {
+            float delta = max - min;
+            saturation = saturation > 0.5 ? delta / (2 - max - min) : delta / (max + min);
+        }
         
-        float lightness = (max - min) / 2;
-        return chroma == 0 ? 0 : chroma / (1 - Mathf.Abs(2 * lightness - 1));
+        return saturation;
     }
 
-    public static float GetValue(this UnityEngine.Color color) => Mathf.Max(Mathf.Max(color.r, color.g), color.b);
+    public static float GetValue(this UnityEngine.Color color) => Mathf.Max(color.r, color.g, color.b);
 
     public static float GetVibrancy(this UnityEngine.Color color)
     {
-        float min = color.MinColorComponent();
-        float max = Mathf.Max(color.maxColorComponent, 0.001f);
+        float min = Mathf.Min(color.r, color.g, color.b);
+        float max = Mathf.Max(color.r, color.g, color.b, 0.001f);
             
         return (max + min) * (max - min) / max;
     }
